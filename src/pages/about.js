5a3f0daf48ecf4019b81a522f193173b640cfab0
Shahnawaz Hossan <s.pabon93@gmail.com>
2019-12-28 00:31:54 +0600

    Styling using CSS Modules added

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,16 @@
 import React from "react"
 import Header from "../components/header"
+import Container from "../components/container"
 
 export default () => (
-  <div style={{ color: `teal` }}>
-    <Header headerText="About Gatsby" />
-    <Header headerText="It's pretty cool" />
-    <p>Such wow. Very React.</p>
-  </div>
+  <Container>
+    <h1>About CSS Modules</h1>
+    <p>CSS Modules are cool</p>
+
+    <div style={{ color: `teal` }}>
+      <Header headerText="About Gatsby" />
+      <Header headerText="It's pretty cool" />
+      <p>Such wow. Very React.</p>
+    </div>
+  </Container>
 )
