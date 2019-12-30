ce7b1a26fdedbedd18f73127b94945f67fbadb60
Shahnawaz Hossan <s.pabon93@gmail.com>
2019-12-30 23:20:29 +0600

    Contact page added, about page edited

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,8 @@
 import React from "react"
-import Header from "../components/header"
-import Container from "../components/container"
 
 export default () => (
-  <Container>
-    <h1>About CSS Modules</h1>
-    <p>CSS Modules are cool</p>
-
-    <div style={{ color: `teal` }}>
-      <Header headerText="About Gatsby" />
-      <Header headerText="It's pretty cool" />
-      <p>Such wow. Very React.</p>
-    </div>
-  </Container>
+  <div>
+    <h1>About me</h1>
+    <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+  </div>
 )
