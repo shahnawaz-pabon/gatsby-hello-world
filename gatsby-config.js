8b5a8ed12b5b08691a16441d69938cb286e440dd
Shahnawaz Hossan <s.pabon93@gmail.com>
2020-01-05 00:10:29 +0600

    Manifest and offline plugin added for pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,5 +24,20 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Learning GatsbyJS`,
+        short_name: `Learning GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
